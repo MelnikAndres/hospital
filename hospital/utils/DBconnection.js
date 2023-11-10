57ddeef43e3b9e3a47c8ace9db3b40340053cf7e
MelnikAndres <104472929+MelnikAndres@users.noreply.github.com>
2023-11-10 17:29:17 -0300

    test workflow

--- hospital/utils/DBconnection.js
+++ hospital/utils/DBconnection.js
@@ -1,7 +1,7 @@
 const pgp = require("pg-promise")();
 const dbConfig = {
     user: 'hospital',
-    database: 'hospital',
+    database: 'hospital-flow',
     password: 'test123',
     host: 'hospital-database', 
     port: 5432,
